92cf13c8e29c77d4e8e69a7d7641b91a18645f75
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-05-19 17:45:54 +0200

    Added searching and listining of ads.

--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,4 +1,10 @@
 import {AdRecord} from "../records/ad.record";
+import { AdEntity } from "../types";
+import { pool } from "../utils/db";
+
+afterAll(async () => {
+    await pool.end();
+})
 
 test('AdRecord returns data from database for one entry', async () => {
     const ad = await AdRecord.getOne('ABC');
@@ -14,4 +20,26 @@ test('AdRecord return null from database for unexisting entry', async () => {
     console.log(ad);
 
     expect(ad).toBeNull();
+})
+
+test('AdRecord.findAll returns array of found entries', async () => {
+    const ads = await AdRecord.findAll('');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+});
+
+test('AdRecord.findAll returns empty array when searching for something that does not exists.', async () => {
+    const ads = await AdRecord.findAll('-----------------------');
+
+    expect(ads).toEqual([]);
+
+})
+
+test('AdRecord.findAll returns small amount of data', async () => {
+    const ads = await AdRecord.findAll('');
+
+    expect((ads[0] as AdEntity).price).toBeUndefined();
+    expect((ads[0] as AdEntity).description).toBeUndefined()
+
 })
